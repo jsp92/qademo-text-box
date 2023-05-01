cdebca294e8b72e3c232f2257cb64f71d5c02b2e
jsp92 <jackson.pace.2@gmail.com>
2023-05-01 11:21:59 -0400

    2nd commit

---
+++ test/specs/example.e2e.js
@@ -0,0 +1,25 @@
+import { faker } from "@faker-js/faker";
+
+describe('Demo QA Text Box', () => {
+    beforeEach(async() => {
+        await browser.url("https://demoqa.com/text-box");
+    });
+    it('Fill out form', async () => {
+        await $("#userName").setValue("testUser");
+        await $("#userEmail").setValue("testEmail123@gmail.com");
+        await $("#currentAddress").setValue("1234 Main St");
+        await $("#permanentAddress").setValue("1234 Main St");
+        await $("#permanentAddress-label").scrollIntoView();
+        await $("#submit").click();
+        await browser.pause(3000);
+
+    it.only("Using faker for the form", async () => {
+        await $("#userName").setValue(faker.internet.userName());
+        await browser.pause(3000);
+    });
+        
+            
+    });
+});
+
+
